ee6fc570937d84cd3ab39b23559de11f83696447
Minh <trinhvanminh2009@gmail.com>
2017-10-25 07:01:53 +0700

    Update paper

--- luanvan/src/translate_vietNamese_chinese/download_application/Test.java
+++ luanvan/src/translate_vietNamese_chinese/download_application/Test.java
@@ -1,18 +1,45 @@
 package translate_vietNamese_chinese.download_application;
 
 import java.io.IOException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import org.jsoup.Jsoup;
-
 
 public class Test {
 
-	
+
+	private static void ToHopKe(int a[], int n, int k) {
+		int i, j, tmp = 0;
+		for (i = 1; i <= k; i++)
+			if (a[i] != n - k + i) {
+				tmp = 1;
+				break;
+			}
+		if (tmp == 0)
+			return;
+		i = k;
+		while (a[i] >= n - k + i)
+			i--;
+		a[i] = a[i] + 1;
+		for (j = i + 1; j <= k; j++)
+			a[j] = a[i] + j - i;
+		for (i = 1; i <= n; i++)
+			System.out.println(a[i]);
+	}
+
+	private static int lt(int n) {
+		if (n == 0) {
+			return 1;
+		} else
+			return (2 * lt(n - 1));
+	}
+
 	public static void main(String[] args) throws IOException {
+		//System.out.println(lt(4));
+		int []a = new int [3];
+		a[0] = 1;
+		a[1] = 1;
+		a[2] = 3;
+
+		ToHopKe(a,5,1);
 		// TODO Auto-generated method stub
-                    System.out.println(Jsoup.connect(URLDecoder.decode("http://cn.news.chinhphu.vn/Home/%e8%b6%8a%e5%8d%97%e5%9b%bd%e5%ae%b6%e4%b8%bb%e5%b8%ad%e9%99%88%e5%a4%a7%e5%85%89%e4%bc%9a%e8%a7%81%e4%bf%9d%e5%8a%a0%e5%88%a9%e4%ba%9a%e9%a9%bb%e8%b6%8a%e5%a4%a7%e4%bd%bf/20179/23310.vgp", "UTF-8")).userAgent("Mozilla").timeout(0).get());
-                System.out.println("translate_vietNamese_chinese.download_application.Test.main()");
 	}
 
 }
