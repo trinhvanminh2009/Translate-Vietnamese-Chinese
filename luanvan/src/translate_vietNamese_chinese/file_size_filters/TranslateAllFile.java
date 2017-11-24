05d9a81a53a019c18a720f682c257fa683b87b8c
Minh <trinhvanminh2009@gmail.com>
2017-11-24 18:34:17 +0700

    Update final translate all file

--- luanvan/src/translate_vietNamese_chinese/file_size_filters/TranslateAllFile.java
+++ luanvan/src/translate_vietNamese_chinese/file_size_filters/TranslateAllFile.java
@@ -25,9 +25,8 @@ public class TranslateAllFile {
 	 */
 	private static void handleFileString(String content, String key, String fileName, String folderName)
 			throws Exception {
-		try{
+		try {
 			int lastIndex = 0;
-
 			ArrayList<Integer> listIndex = new ArrayList<>();
 			while (lastIndex != -1) {
 				lastIndex = content.indexOf(key, lastIndex);
@@ -38,12 +37,14 @@ public class TranslateAllFile {
 
 				}
 			}
-			// System.out.println(content.length());
+			// Starting cut content in main paper.
 			lastIndex = 0;
 			String title = content.substring(listIndex.get(0), listIndex.get(1));
 			String mainContent = content.substring(listIndex.get(1), listIndex.get(2));
-			ArrayList<String> listMainContent = new ArrayList<>();
+			ArrayList<String> listMainContentCutted = new ArrayList<>();
+			ArrayList<String> listMainContentTranslated = new ArrayList<>();
 			ArrayList<Integer> listIndexMainContent = new ArrayList<>();
+
 			for (int i = 0; i < mainContent.length(); i++) {
 				lastIndex = mainContent.indexOf("。", lastIndex);
 				if (lastIndex != -1) {
@@ -56,53 +57,45 @@ public class TranslateAllFile {
 				lastIndex++;
 			}
 			System.out.println(fileName);
+			// Get content by sentences
 			if (listIndexMainContent.size() > 0) {
-				System.out.println(mainContent.substring(0, listIndexMainContent.get(0)));
+				listMainContentCutted.add(mainContent.substring(0, listIndexMainContent.get(0)));
 				for (int i = 0; i < mainContent.length(); i++) {
-					System.out.println(listIndexMainContent.get(i));
-					System.out.println("size: " + listIndexMainContent.size());
-					if (returnIndexOfElementInArrrayList(listIndexMainContent,
+					if (getIndexOfElementInArrrayList(listIndexMainContent,
 							listIndexMainContent.get(i)) == listIndexMainContent.size() - 1) {
 						break;
 					} else {
-						System.out.println(
-								"Substring from " + listIndexMainContent.get(i) + " to " + listIndexMainContent.get(i + 1));
 						if (listIndexMainContent.get(i) == listIndexMainContent.get(i + 1)) {
 							break;
 						} else {
-							System.out.println(
-									mainContent.substring(listIndexMainContent.get(i), listIndexMainContent.get(i + 1)));
+							listMainContentCutted.add(mainContent.substring(listIndexMainContent.get(i),
+									listIndexMainContent.get(i + 1)));
 						}
 					}
 				}
 			}
-		}catch (Exception e) {
+			// Starting translate each sentence
+			TranslateAllFile http = new TranslateAllFile();
+			listMainContentTranslated.add(http.callUrlAndParseResult("zh-CN", "vi",title));
+			listMainContentTranslated.add(key);
+			for (int i = 0; i < listMainContentCutted.size(); i++) {
+				listMainContentTranslated.add(http.callUrlAndParseResult("zh-CN", "vi",
+						listMainContentCutted.get(i).trim().replace("。", "")));
+			}
+			//Convert list 
+			String contentVN = listMainContentTranslated.get(0) + listMainContentTranslated.get(1)+"\n" ;
+			for(int i = 2 ; i < listMainContentTranslated.size(); i++){
+				contentVN = contentVN + listMainContentTranslated.get(i) + ".\n" ;
+			}
+			System.out.println(contentVN);
+			Translate.writeTranstedToText(folderName, fileName, contentVN);
+
+		} catch (Exception e) {
 			// TODO: handle exception
 		}
-	
-
-		// System.out.println(mainContent.length());
-		// mainContent = mainContent.replace(key, "");
-		/*
-		 * String test =
-		 * "越共中央经济部部长会见越南欧洲商会制药协会区域高级代表团（图片来源：越通社） 越通社河内—— 越共中央政治局委员、中央书记处书记、中央经济部长阮文平会见越南欧洲商会制药协会区域高级代表团。  阮文平向越南欧洲商会制药协会区域高级代表团介绍越南党和政府为改善营商环境和提高竞争力所采取的大方向和措施，旨在鼓励企业扩大包括制药领域在内的各领域经营投资合作。他表示，越南十分重视保持社会政策和谐，努力让群众用低廉的费用获取优质的医疗卫生服务。 阮文平强调， 越南政府愿为包括欧洲商会企业在内的各国制药企业扩大在越投资并设立药物研发中心，同越南企业合作，将越南发展成为东南亚地区制药基地等创造便利条件。 越南欧洲商会制药协会副会长Nicholas Jones对越南经济社会发展前景以及越南政府为改善营商环境、简化行政审批制度等充满信心。 欧洲企业对越南宏观经济的稳定充满信心 他对越南在制药和医疗卫生体系所遇到的困难和挑战表示理解，同时希望越南更加努力改善法律框架和政策机制，为从事制药等领域的企业创造更为便利的条件。 越南欧洲商会制药企业协会多家企业代表一致认为越南有望成为东盟的制药生产基地，同时就有关医药行业发展模式、促进患者在药品领域的可持续性获得、政府采购、知识产权保护、对越投资机会等问题进行探讨。（越通社——VNA） "
-		 * ; TranslateAllFile http = new TranslateAllFile();
-		 * System.out.println(test.length()); String wordToEnglish =
-		 * http.callUrlAndParseResult("zh-CN", "vi", test);
-		 * System.out.println(wordToEnglish);
-		 */
-		/*
-		 * String wordEnglishToVietnamese = http.callUrlAndParseResult("en",
-		 * "vi", wordToEnglish); System.out.println(wordEnglishToVietnamese);
-		 */
-
-		/*
-		 * System.out.println(fileName); System.out.println(wordToEnglish);
-		 * Translate.writeTranstedToText(folderName, fileName, wordToEnglish);
-		 */
 	}
 
-	private static int returnIndexOfElementInArrrayList(ArrayList<Integer> arrayList, int currentNumber) {
+	private static int getIndexOfElementInArrrayList(ArrayList<Integer> arrayList, int currentNumber) {
 		for (int i = 0; i < arrayList.size(); i++) {
 			if (currentNumber == arrayList.get(i)) {
 				return i;
